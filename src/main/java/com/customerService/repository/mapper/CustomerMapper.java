bcd426112124e4553c910a8f845c313b6ba0df6e
AvichaiCohen <stiven962011@gmail.com>
2022-11-07 21:28:21 +0200

    third_project

--- src/main/java/com/customerService/repository/mapper/CustomerMapper.java
+++ src/main/java/com/customerService/repository/mapper/CustomerMapper.java
@@ -7,7 +7,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CustomerMapper implements RowMapper<Customer> {
-
     @Override
     public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
         return new Customer(
